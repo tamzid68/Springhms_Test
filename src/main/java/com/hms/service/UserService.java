64de0be7a6a832e7502d545a913ccf3faf09a845
ASM Tamzid <atkthegreat9999@gmail.com>
2024-09-21 21:17:13 +0600

    added show all info

--- src/main/java/com/hms/service/UserService.java
+++ src/main/java/com/hms/service/UserService.java
@@ -5,11 +5,18 @@ import com.hms.repository.UserJPARepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService implements UserServiceIf {
 @Autowired
 UserJPARepo userRepo;
-@Override
+
+    public List<User> getUserAll() {
+        return userRepo.findAll();
+    }
+
+    @Override
 public User saveUser(User user) {
 
         return userRepo.save(user);
